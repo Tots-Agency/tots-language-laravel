e272b62d3473b2423cb68e788503571cf555aeda
Matias Camiletti <matias.camiletti@gmail.com>
2024-04-22 12:29:03 -0300

    Add provider.

---
+++ src/Providers/LanguageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tots\Device\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LanguageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register migrations
+        if($this->app->runningInConsole()){
+            $this->registerMigrations();
+        }
+    }
+    /**
+     * Register migrations library
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        $mainPath = database_path('migrations');
+        $paths = array_merge([
+            './vendor/tots/language-laravel/database/migrations'
+        ], [$mainPath]);
+        $this->loadMigrationsFrom($paths);
+    }
+
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
